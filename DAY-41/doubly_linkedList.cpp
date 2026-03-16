709dd1016a974caa48bdc43a131cd5b09c23c4a8
srikanta2006 <srikanta2006@users.noreply.github.com>
2026-03-16 19:16:57 +0530

    DAY-41

---
+++ DAY-41/doubly_linkedList.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+
+    Node(int val){
+        data = val;
+        next=prev=NULL;
+    }
+};
+
+class DoublyList{
+    Node* head;
+    Node* tail;
+
+    public:
+        DoublyList(){
+            head=tail=NULL;
+        }
+        void push_front(int val){
+            Node* newNode = new Node(val);
+            if(head==NULL){
+                head=tail=newNode;
+            }
+            else{
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+            }
+        }
+
+        void push_back(int val){
+            Node* newNode = new Node(val);
+            if(tail == NULL){
+                head=tail=newNode;
+            }
+            else{
+                tail->next = newNode;
+                newNode->prev = tail;
+                tail = newNode;
+            }
+        }
+        
+        void pop_front(){
+            if(head==NULL){
+                cout<<"DLL is empty"<<endl;
+                return;
+            }
+            Node* temp = head;
+            head=head->next;
+            if(head!=NULL){
+                head->prev = NULL;
+            }
+            temp->next = NULL;
+            delete temp;
+        }
+
+        void pop_back(){
+             if(head==NULL){
+                cout<<"DLL is empty"<<endl;
+                return;
+            }
+            Node* temp =tail;
+            tail=tail->prev;
+            if(tail!=NULL){
+                tail->next =NULL;
+            }
+            temp->prev = NULL;
+            delete temp;
+        }
+        void display(){
+            Node* temp = head;
+            while(temp!=NULL){
+                cout<<temp->data << " <=> ";
+                temp=temp->next;
+            }
+            cout<<"NULL"<<endl;
+        }
+};
+
+
+int main(){
+    DoublyList dll;
+    dll.push_front(23);
+    dll.push_front(24);
+    dll.push_front(25);
+    dll.push_back(26);
+    dll.display();
+    
+    return 0;
+}
