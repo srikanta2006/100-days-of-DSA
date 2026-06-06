95b8e6346ccbfa0fc329632908226f8743493828
srikanta2006 <srikanta2006@users.noreply.github.com>
2026-06-06 19:07:04 +0530

    Boiler_plate_codes

---
+++ Boiler_plates/Tree_boiler_plate.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+TreeNode* createTree(){
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->left->right->right = new TreeNode(6);
+    return root;
+}
+
+//write your functions here
+
+int main(){
+    TreeNode* root = createTree();
+    //write your code here
+    return 0;
+}
